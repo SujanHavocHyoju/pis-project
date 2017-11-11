2e8fb5f021dbe5fba64888665f5d58f0817bf5b6
lizeshakya <lizeshakya@gmail.com>
2017-11-11 13:13:40 +0545

    Activity wise report and office wise report is developed

--- cp/activitywise.php
+++ cp/activitywise.php
@@ -1,83 +1,61 @@
-<?php
-session_start();
-include('../class/common.php');
-require_once "../class/PHPExcel.php";
-if(isset($_SESSION['user_type'])){
-    if($_SESSION['user_type']==0){
-$objPHPExcel = new PHPExcel();
-$office_name = 'कार्यक्रम अनुसर';
-$fiscal_year = '2074/75';
-ini_set('max_execution_time', 300);
-$sql = $dbc->selectTransactionByActivity();
-$objPHPExcel->setActiveSheetIndex()
-->mergeCells('C3:C4');
-$objPHPExcel->setActiveSheetIndex()
-->mergeCells('D1:I1');
-$objPHPExcel->setActiveSheetIndex()
-->mergeCells('I3:J3');
-$objPHPExcel->setActiveSheetIndex()
-->mergeCells('K3:L3');
-$objPHPExcel->setActiveSheetIndex()->mergeCells('D2:I2');
+<?php $sql = $dbc->selectAllActivities() ?>
 
-$objPHPExcel->setActiveSheetIndex()->setCellValue('D1',$office_name);
-$objPHPExcel->setActiveSheetIndex()->mergeCells('D2:I2');
-$objPHPExcel->setActiveSheetIndex()->setCellValue('D2',$fiscal_year);
-$objPHPExcel->setActiveSheetIndex()->mergeCells('A3:A4');
-$objPHPExcel->setActiveSheetIndex()->setCellValue('A3','कार्यक्रम सँकेत न.');
-$objPHPExcel->setActiveSheetIndex()->mergeCells('B3:B4');
-$objPHPExcel->setActiveSheetIndex()->setCellValue('B3','कार्यक्रम/क्रियाकलाप')
-->setCellValue('C3','इकाई');
-$objPHPExcel->setActiveSheetIndex()->mergeCells('D3:F3');
+<?php if (isset($_POST['btnfind'])) {
+    $aid = $_POST['activitycode'];
+    $acode = mysqli_fetch_assoc($dbc->selectActivityCode($aid));
+    echo "<script>location.href='http://localhost/pis-project/cp/dashboard.php?action=activitywisereport&aid=" . $aid . "&acode=" . $acode['code']. "';</script>";
+} ?>
 
-$objPHPExcel->setActiveSheetIndex()->setCellValue('D3','वार्षिक लक्ष')
-->setCellValue('D4', 'भौतिक परिमाण')
-->setCellValue('E4', 'इकाइ लागत')
-->setCellValue('F4','बजेट');
-$objPHPExcel->setActiveSheetIndex()->mergeCells('G3:H3');
-$objPHPExcel->setActiveSheetIndex()->setCellValue('G3',"वार्षिक प्रगति")
-->setCellValue('G4',"भौतिक परिमाण")
-->setCellValue('h4','खर्च बजेट')
-->setCellValue('I3','प्रथम चौमासिक लक्ष')
-->setCellValue('I4','प्रथम चौमासिक लक्ष')
-->setCellValue('J4','बजेट')
-->setCellValue('K3','प्रथम चौमासिक प्रगति')
-->setCellValue('k4','भौतिक परिमाण')
-->setCellValue('L4','खर्च बजेट');;
+<div id="skip-menu"></div>
 
-$i=4;
-while($row = mysqli_fetch_array($sql)) {
-    $i++;
-    $objPHPExcel->setActiveSheetIndex()
-    ->setCellValue('A'.$i,$row['code'])
-    ->setCellValue('B'.$i,$row['name_np'])
-    ->setCellValue('C'.$i,"")
-    ->setCellValue('D'.$i,$row['yearly_alloc_qty'])
-    ->setCellValue('E'.$i,$row['yearly_alloc_cost'])
-    ->setCellValue('F'.$i,$row['yearly_alloc_budget'])
-    ->setCellValue('G'.$i,$row['yearly_progress_qty'])
-    ->setCellValue('H'.$i,$row['yearly_progress_expenditure'])
-    ->setCellValue('I'.$i,$row['q1_alloc_qty'])
-    ->setCellValue('J'.$i,$row['q1_alloc_budget'])
-    ->setCellValue('K'.$i,$row['q1_progress_qty'])
-    ->setCellValue('L'.$i,$row['q1_progress_expenditure']);
-}
-header('Content-Type: application/vnd.ms-excel');
-header('Content-Disposition: attachment;filename="pis-report-for-'.$office_name.'.xls"');
-header('Cache-Control: max-age=0');
-// If you're serving to IE 9, then the following may be needed
-header('Cache-Control: max-age=1');
-// If you're serving to IE over SSL, then the following may be needed
-header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-header ('Pragma: public'); // HTTP/1.0
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-$objWriter->save('php://output');
-}else{
-    echo "<script>location.href='http://localhost/pis-project/cp/dashboard.php?action=home';</script>";
-}
-}
-else{
-echo "<script>location.href='http://localhost/pis-project/cp/dashboard.php?action=home';</script>";
-} 
-?>
+<!-- Content box -->
+<div id="content-box">
+    <div id="content-box-in">
+
+        <!-- Content left -->
+        <div id="content-box-in-left">
+            <div id="content-box-in-left-in">
+                <h3 class="line"><span class="preeti" style="font-size:23px">क्रियाकलाप छान्नुहोस</span></h3>
+
+                <!-- My latest work -->
+                <div class="galerie">
+
+
+                    <form name="activitywise" method="POST">
+                        <table align="center" width="50%" class="table">
+                            <tr>
+                                <td width="50%" align="right"><p class="preeti">क्रियाकलाप &nbsp;</p></td>
+                                <td>:</td>
+                                <td width="50%">
+                                    <select name="activitycode" class="preeti" style="width:300px; ">
+                                        <?php while ($row = mysqli_fetch_array($sql)) { ?>
+                                            <option value="<?php echo $row['id'] ?>"><?php echo $row['code'] . ' ' . $row['name_np'] ?></option>
+                                        <?php } ?>
+                                    </select>
+
+                                </td>
+                            </tr>
+
+
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td></td>
+                                <td><input type="submit" name="btnfind" value="  प्रतिवेदन हेर्ने  "/></td>
+                            </tr>
+                        </table>
+                    </form>
+                    <br/>
+                    <div class="cleaner">&nbsp;</div>
+                </div>
+                <!-- My latest work end -->
+
+
+            </div>
+        </div>
+        <!-- Content left end -->
+
+        <!-- Content right --><!-- Content right end -->
+        <div class="cleaner">&nbsp;</div>
+    </div>
+</div>
+<!-- Content box end -->
---
+++ cp/activitywisereport.php
@@ -0,0 +1,175 @@
+<?php
+
+$activity_id = $_GET['aid'];
+$activity_code = $_GET['acode'];
+$activity = mysqli_fetch_array($dbc->selectOneActivity($activity_id));
+
+
+?>
+
+
+<div id="content-box">
+    <div id="content-box-in">
+
+        <!-- Content left -->
+        <div id="content-box-in-left">
+            <div id="content-box-in-left-in">
+
+                <h3 align="center"><span class="preeti" style="font-size:20px; text-align: center">
+                     नेपाल सरकार
+                     शिक्षा मन्त्रालय
+                     शिक्षा विभाग
+
+                    </span>
+                </h3>
+
+                <h3 class="line">
+                    <span class="preeti" style="font-size:23px;">
+                    </span>
+                </h3>
+
+                <!-- My latest work -->
+                <div class="galerie preeti">
+
+                    <table border="0" cellpadding="0" cellspacing="0" style="width: 100%;">
+                        <tbody>
+                        <tr>
+
+                            <td colspan="14" style="text-align: center;">
+                                प्रगति प्रतिवेदन
+                            </td>
+                        </tr>
+                        <tr>
+
+                            <td colspan="14" style="text-align: center;">
+                                आ.व. &#2408;&#2406;&#2413;&#2409;/&#2413;&#2410; (जिल्लास्तर)
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colspan="14">कार्यक्रम : ३५०८०६ (विद्यालयक्षेत्र विकास कार्यक्रम)</td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+                    <p>&nbsp;क्रियाकलाप : <?php echo $activity['code'] . ' ' . $activity['name_np'] ?></p>
+                    <br>
+
+
+                    <form name="del" action="" method="post">
+                        <table width="100%" align="center" border="1" class="table">
+                            <tr>
+                                <td rowspan="2" align="center"><span class="preeti">सि. न.</span></td>
+                                <td rowspan="2" align="center"><span class="preeti">कार्यक्रम/क्रियाकलाप</span></td>
+                                <td rowspan="2" align="center"><span class="preeti">एकाई</span></td>
+                                <td colspan="3" align="center"><span class="preeti">वार्षिक लक्ष</span></td>
+                                <td colspan="5" align="center"><span class="preeti">वार्षिक प्रगति</span></td>
+                                <td colspan="3" align="center"><span class="preeti">तेश्रो चौमासिक लक्ष</span></td>
+                                <td colspan="4" align="center"><span class="preeti">तेश्रो चौमासिक प्रगति</span></td>
+
+
+                            </tr>
+                            <tr>
+                                <td align="center"><span class="preeti">इकाइ लागत</span></td>
+                                <td align="center"><span class="preeti">भौतिक परिमाण</span></td>
+                                <td align="center"><span class="preeti">भार</span></td>
+                                <td align="center"><span class="preeti">बजेट</span></td>
+
+                                <td align="center"><span class="preeti">भौतिक परिमाण</span></td>
+                                <td align="center"><span class="preeti">भौतिक प्रगति प्रतिशत</span></td>
+                                <td align="center"><span class="preeti">खर्च रकम</span></td>
+                                <td align="center"><span class="preeti">खर्च प्रतिशत</span></td>
+
+
+                                <td align="center"><span class="preeti">भौतिक परिमाण</span></td>
+                                <td align="center"><span class="preeti">भार</span></td>
+                                <td align="center"><span class="preeti">बजेट</span></td>
+
+                                <td align="center"><span class="preeti">भौतिक परिमाण</span></td>
+                                <td align="center"><span class="preeti">भौतिक प्रगति प्रतिशत</span></td>
+                                <td align="center"><span class="preeti">खर्च रकम</span></td>
+                                <td align="center"><span class="preeti">खर्च प्रतिशत</span></td>
+
+
+                            </tr>
+
+
+                            <?php $sql = $dbc->selectActivityWiseReport($activity_code);
+                            $i = 1;
+                            while ($row = mysqli_fetch_array($sql)) {
+                                ?>
+
+                                <tr>
+                                    <?php
+                                    $percentageYearlyQty = $row['yearly_alloc_qty'] == 0 || $row['yearly_progress_qty'] ? 0 : $row['yearly_progress_qty'] / $row['yearly_alloc_qty'] * 100;
+                                    $percentageYearlyExpenditure = $row['yearly_alloc_budget'] == 0 || $row['yearly_progress_expenditure'] ? 0 : $row['yearly_progress_expenditure'] / $row['yearly_alloc_budget'] * 100;
+                                    $percentageQThreeQty = $row['q3_alloc_qty'] == 0 || $row['q3_progress_qty'] ? 0 : $row['q3_progress_qty'] / $row['q3_alloc_qty'] * 100;
+                                    $percentageQThreeExpenditure = $row['q3_alloc_budget'] == 0 || $row['q3_progress_expenditure'] ? 0 : $row['q3_progress_expenditure'] / $row['q3_alloc_budget'] * 100;
+                                    ?>
+
+                                    <td><span class="siddhi"><?php echo $i++ ?></span></td>
+                                    <td><span class="siddhi"><?php echo $row['name_np'] ?></span></td>
+                                    <td><span class="siddhi"></span></td>
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $row['yearly_alloc_cost'] ?></span></td>
+                                    <td align="right"><span class="preeti"><?php echo $row['yearly_alloc_qty'] ?></span>
+                                    </td>
+                                    <td align="right"><span class="preeti"></span></td>
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $row['yearly_alloc_budget'] ?></span></td>
+
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $row['yearly_progress_qty'] ?></span>
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $percentageYearlyQty ?></span>
+                                    </td>
+
+
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $row['yearly_progress_expenditure'] ?></span>
+                                    </td>
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $percentageYearlyExpenditure?></span>
+                                    </td>
+
+
+                                    <td align="right"><span class="preeti"><?php echo $row['q3_alloc_qty'] ?></span>
+                                    </td>
+
+                                    <td align="right"><span class="preeti"></span></td>
+                                    <td align="right"><span class="preeti"><?php echo $row['q3_alloc_budget'] ?></span>
+                                    </td>
+
+                                    <td align="right"><span class="preeti"><?php echo $row['q3_progress_qty'] ?></span>
+                                    </td>
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $percentageQThreeQty ?></span>
+                                    </td>
+
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $row['q3_progress_expenditure'] ?></span></td>
+                                    <td align="right"><span
+                                                class="preeti"><?php echo $percentageQThreeExpenditure ?></span>
+                                    </td>
+
+                                </tr>
+
+                            <?php } ?>
+
+
+                        </table>
+                    </form>
+                    <br/>
+                    <div class="cleaner">&nbsp;</div>
+                </div>
+                <!-- My latest work end -->
+            </div>
+        </div>
+        <!-- Content left end -->
+
+        <!-- Content right --><!-- Content right end -->
+        <div class="cleaner">&nbsp;</div>
+    </div>
+</div>
+
+
